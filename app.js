d0a480fc5dac4f1a1cb7579dddd4cae00b356f88
Hassan Sarhan <hasoun7772@gmail.com>
2019-01-01 08:25:43 -0500

    Finalized project

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const cityName = document.getElementById("city");
+const cityName = document.getElementById("newCity");
 const changeLocation = document.getElementById("submit");
+const city = document.getElementById("city");
+const status = document.getElementById("weatherStatus");
+const degrees = document.getElementById("degrees");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const icon = document.getElementById("icon");
 const defaultCity = "London";
 
 let weather = new Weather(defaultCity);
-weather.getStats();
+weather.updateStats();
+changeCity(defaultCity);
 
 function showError() {
 	let err = document.getElementById("error");
@@ -14,9 +21,16 @@ function showError() {
 	}, 3000);
 }
 
-changeLocation.addEventListener("click", () => {
+changeLocation.addEventListener("click", changeCity);
+
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function changeCity() {
 	if (cityName.value != "") {
-		weather = new Weather(cityName.value);
-		weather.getStats();
+		weather = new Weather(capitalizeFirstLetter(cityName.value));
+
+		weather.updateStats();
 	}
-});
+}
--- weather.js
+++ weather.js
@@ -2,10 +2,11 @@
 class Weather {
 	constructor(city) {
 		this.city = city;
+		this.data = [];
 		this.apiKey = "abae64df06242e83938d6e36efd8b60f";
 	}
 
-	getStats() {
+	updateStats() {
 		let apiRes = fetch(
 			`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${
 				this.apiKey
@@ -15,17 +16,28 @@ class Weather {
 			let weatherData = res.json();
 			weatherData
 				.then(data => {
-					console.log(data);
-					let a = [];
-					a.push(this.city);
-					a.push(data.weather[0].main);
-					a.push(data.main.temp - 273);
-					a.push(data.weather[0].icon);
-					a.push(data.main.humidity);
-					a.push(data.wind.speed);
-					return a;
+					this.data.push(this.city);
+					this.data.push(data.weather[0].main);
+					this.data.push(data.main.temp - 273);
+					this.data.push(data.weather[0].icon);
+					this.data.push(data.main.humidity);
+					this.data.push(data.wind.speed);
+					console.log(this.data);
+				})
+				.then(() => {
+					let a = this.data;
+					city.textContent = a[0];
+					status.textContent = a[1];
+					degrees.textContent = `${Math.round(a[2])} C`;
+					icon.setAttribute(
+						"src",
+						`http://openweathermap.org/img/w/${a[3]}.png`
+					);
+					humidity.textContent = `Relative Humidity: ${a[4]}%`;
+					windSpeed.textContent = `Wind Speed: ${a[5]}m/s`;
 				})
 				.catch(err => {
+					console.log(err);
 					showError(err);
 				});
 		});
