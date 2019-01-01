eeec66881f177cce33c4093715ace7e81d7a4c9a
Hassan Sarhan <hasoun7772@gmail.com>
2019-01-01 06:46:30 -0500

    Added modal

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const city = "London";
+const cityName = document.getElementById("city");
+const changeLocation = document.getElementById("submit");
+const defaultCity = "London";
 
-const weather = new Weather(city);
+let weather = new Weather(defaultCity);
 weather.getStats();
+
+function showError() {
+	let err = document.getElementById("error");
+	err.className = "alert alert-danger";
+	err.textContent = "City not found";
+	setTimeout(() => {
+		err.className = "alert alert-danger d-none";
+	}, 3000);
+}
+
+changeLocation.addEventListener("click", () => {
+	if (cityName.value != "") {
+		weather = new Weather(cityName.value);
+		weather.getStats();
+	}
+});
